b1de95ae226c46c5dfcebbdc58c01228185f11a0
Kira <kira.kaur.sidhu@gmail.com>
2023-02-24 12:30:32 -0500

    Create problem7.py

---
+++ 6.100A/midterm/problem7.py
@@ -0,0 +1,27 @@
+def score(word,f):
+    """
+       word, a string of length > 1 of alphabetical 
+             characters (upper and lowercase)
+       f, a function that takes in two int arguments and returns an int
+
+       Returns the score of word as defined by the method:
+
+    1) Score for each letter is its location in the alphabet (a=1 ... z=26) 
+       times its distance from start of word.  
+       Ex. the scores for the letters in 'adD' are 1*0, 4*1, and 4*2.
+    2) The score for a word is the result of applying f to the
+       scores of the word's two highest scoring letters. 
+       The first parameter to f is the highest letter score, 
+       and the second parameter is the second highest letter score.
+       Ex. If f returns the sum of its arguments, then the 
+           score for 'adD' is 12 
+    """
+    s = word.lower()
+    scores = []
+
+    for i in range(len(s)):
+        value = ord(s[i]) - 96
+        scores.append(value*i)
+
+    scores.sort()
+    return f(scores[len(s)-1], scores[len(s)-2])
